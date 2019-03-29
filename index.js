7673cc02873be350a5965c69c19e39f8bd2cb3aa
Hanna Engel <hengel@alumni.berklee.edu>
2019-03-29 01:46:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var name = "Joe"
+var name = "susan"
 var height = "74"
 
-
+if (name==="susan"){
+  console.log("name is susan")
+} else {
+  console.log("name isn't susan")
+}
 // Don't worry about this
 module.exports = { name, height
 }
-
